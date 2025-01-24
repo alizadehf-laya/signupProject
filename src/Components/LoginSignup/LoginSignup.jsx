a076ea7e5468068333d6e39cd0ba07962c48ead0
alizadehf-laya <145831251+alizadehf-laya@users.noreply.github.com>
2025-01-23 23:37:19 -0800

    input designed

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,28 +1,33 @@
-import './LoginSignup.css'
+import styles from './LoginSignup.module.css'
 import password_icon from './Assets/password.jfif'
 import email_icon from './Assets/email.jfif'
 import user_icon from './Assets/OIP.jfif'
 
 const LoginSignup = () =>{
     return(
-        <div className='container'>
-      <div className="header">
-        <div className="text">sign up</div>
-        <div className="underline"></div>
+        <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.text}>sign up</div>
+        <div className={styles.underline}></div>
       </div>
-      <div className="inputs">
-        <div className="input">
-            <img src={user_icon} alt=""/>
-            <input type="text"/>
+      <div className={styles.inputs}>
+        <div className={styles.input}>
+            <img src={user_icon} alt="" className={styles.img1}/>
+            <input type="text" placeholder='Name'/>
         </div>
-        <div className="input">
-            <img src={email_icon} alt=""/>
-            <input type="email"/>
+        <div className={styles.input}>
+            <img src={email_icon} alt="" className={styles.img1}/>
+            <input type="email" placeholder='Email Id'/>
         </div>
-        <div className="input">
-            <img src={password_icon} alt=""/>
-            <input type="password"/>
+        <div className={styles.input}>
+            <img src={password_icon} alt="" className={styles.img1}/>
+            <input type="password"  placeholder='Password'/>
         </div>
+      </div>
+      <div className="forget-password">Lost Password?<spasn>Click Here!</spasn></div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Log in</div>
       </div>
         </div>
     )
