eb247d13bb5532316f76cdc3c497b2e08f021fe8
alizadehf-laya <145831251+alizadehf-laya@users.noreply.github.com>
2025-01-24 05:36:09 -0800

    login added

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -2,12 +2,49 @@ import styles from './LoginSignup.module.css'
 import password_icon from './Assets/password.jfif'
 import email_icon from './Assets/email.jfif'
 import user_icon from './Assets/OIP.jfif'
+import { useState } from 'react'
 
 const LoginSignup = () =>{
+    const submit={
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "220px",
+        height: "59px",
+        color: "#fff",
+        background: "#42006c",
+        borderRadius: "50px",
+        fontSize: "19px",
+        fontWeight: "700",
+        cursor: "pointer"
+    }
+    const submit1={
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "220px",
+        height: "59px",
+        color: "#fff",
+        background: "#42006c",
+        borderRadius: "50px",
+        fontSize: "19px",
+        fontWeight: "700",
+        cursor: "pointer"
+    }
+    const text={
+        color:"#3c009d",
+        fontSize:" 48px",
+        fontWeight: "600",
+    }
+    const gray={
+        background:"#eaeaea",
+        color: "#676767",
+    }
+    const [action,setAction] = useState("Sign Up");
     return(
         <div className={styles.container}>
       <div className={styles.header}>
-        <div className={styles.text}>sign up</div>
+        <div  style={text}>{action}</div>
         <div className={styles.underline}></div>
       </div>
       <div className={styles.inputs}>
@@ -24,10 +61,10 @@ const LoginSignup = () =>{
             <input type="password"  placeholder='Password'/>
         </div>
       </div>
-      <div className="forget-password">Lost Password?<spasn>Click Here!</spasn></div>
-      <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Log in</div>
+      <div className={styles.forget_password}>Lost Password?<span>Click Here!</span></div>
+      <div className={styles.password_container}>
+        <div style={submit} className={action==="login"?"submit gray" : "submit"} onClick={()=>{setAction("sign up")}}>Sign Up</div>
+        <div style={submit}  className={action==="Sign Up"?"submit gray": "submit"}onClick={()=>{setAction("Login")}}>Login</div>
       </div>
         </div>
     )
